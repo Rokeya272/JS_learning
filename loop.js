8130f2987090085dbdd1b5abca5dca873f9b1c0e
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-07 15:05:14 +0600

    loops_all

---
+++ loop.js
@@ -0,0 +1,15 @@
+//initial
+/*for(let i=0;i<7;i++){
+    console.log("hello : ",i);
+}*/
+
+//square
+/*for(let j=1;j<=5;j++){
+    console.log(`${j}x${j} = ${j*j}`);
+}*/
+
+//decrement
+for(let i=10;i>0;i-=1){
+     console.log(i);
+}
+
