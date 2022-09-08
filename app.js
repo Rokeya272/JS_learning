4f19fbb5401c6c3398e51f85e2cfb9e7b51cd592
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-09 02:37:37 +0600

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const target = Math.floor(Math.random()*10);
+/*const target = Math.floor(Math.random()*10);
 let guess = Math.floor(Math.random()*10);
 
 while(guess!== target){
     console.log(`target : ${target} ; guess : ${guess}`);
+    guess = Math.floor(Math.random()*10);
+}
+
+console.log(`target : ${target} guess: ${guess}`);
+console.log(`CONGRATS YOU HAVE WON THE MATCH!`);*/
+
+///use of break
+const target = Math.floor(Math.random()*10);
+let guess = Math.floor(Math.random()*10);
+
+while(true){
+
+    if(guess === target)break;
     guess = Math.floor(Math.random()*10);
 }
 
---
+++ forof.js
@@ -0,0 +1,26 @@
+let team = ['football','cricket','tennis'];
+
+for(let p of team){
+    console.log(p);
+}
+
+for(let char of 'chicken'){
+    console.log(char.toUpperCase());
+}
+
+// check all row sum are equal
+
+let square = [[2,7,6],[9,5,1],[4,3,8]];
+
+for(let s of square ){
+    let sum = 0;
+
+    for(let q of s){
+        sum = sum+q;
+        
+    }
+    console.log(`${s} summed ${sum}`);
+
+    
+    
+}
