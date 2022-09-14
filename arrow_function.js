b445f8309051df308140721cf0107c93a2200f29
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-14 22:30:09 +0600

    updated

---
+++ arrow_function.js
@@ -0,0 +1,30 @@
+/*const square = (x)=>{
+    console.log(x*x);
+}
+
+square(5);*/
+
+/*const iseven=(num)=>{
+
+    if(num%2 == 0){
+        console.log('True');
+    }
+    else{
+        console.log('False');
+    }
+}
+
+iseven(32);
+iseven(19);*/
+
+/*const multiply = (a,b) =>{
+    console.log(a*b);
+}
+
+multiply(3,4);*/
+
+const greet = () =>{
+    console.log('HI');
+}
+
+greet();
