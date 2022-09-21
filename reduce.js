ff26ee148420b7e90b205ce83fe1ced44f4ef9f4
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-21 23:16:46 +0600

    updated

--- reduce.js
+++ reduce.js
@@ -7,7 +7,7 @@ const prod = nums.reduce((total,currentval)=>{
 
 ///finding maximum value
 
-const grades = [67,84,97,92,88,99,73,70,64];
+//const grades = [67,84,97,92,88,99,73,70,64];
 
 /*const maximum = grades.reduce((max,currentval)=>{
     if(currentval>max) return currentval;
@@ -20,8 +20,27 @@ const grades = [67,84,97,92,88,99,73,70,64];
     return Math.max(max,currentval);
 })*/
 
-const minimum = grades.reduce((min,currentval)=>{
+/*const minimum = grades.reduce((min,currentval)=>{
     return Math.min(min,currentval);
-})
+})*/
+
+
+///counting elements
+
+const votes = ['y','y','n','y','n','n','y','y','y','y','n'];
+/*const result = votes.reduce((tally,val)=>{
+    if(tally[val]){tally[val]++;}
+    else{tally[val] = 1;}
+    return tally;
+},{})*/
+
+const result2 = votes.reduce((tally,val)=>{
+
+    tally[val]=(tally[val]||0)+1;
+    return tally;
+},{})
+
+
+
 
 
