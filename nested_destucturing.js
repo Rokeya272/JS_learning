840675ae4581bfff9674a1785adeaf95e46e7e7a
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-28 23:59:14 +0600

    updated

---
+++ nested_destucturing.js
@@ -0,0 +1,16 @@
+const result = [{
+    first : 'Eliud',
+    last : 'kipchoge',
+    country : 'kenya'
+
+},
+{
+    first : 'Feyisa',
+    last : 'lilesa',
+    country :'ethiopia'
+
+}]
+
+const[{first : goldwinner},{country}] = result;
+
+console.log(goldwinner);
