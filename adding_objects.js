479366e2d66c145150be90bb8fa19e091426a4eb
Rokeya272 <rokeyafabby60@gmail.com>
2022-09-28 23:57:43 +0600

    updated

---
+++ adding_objects.js
@@ -0,0 +1,12 @@
+const math = {
+    numbers : [1,2,3,4,5,6],
+    add : function(x,y){
+        return x+y;
+    },
+
+    multiply : function(x,y){
+        return x*y;
+
+    }
+}
+console.log(math.multiply(5,3));
